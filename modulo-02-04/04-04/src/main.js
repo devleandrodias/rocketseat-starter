90b0b0a9f3bfd77f7d7d1472c7eb402e6f191e02
devleandrodias <leandro.dias_profissional@outlook.com.br>
2019-11-15 14:30:57 -0300

    starter

--- modulo-02-04/04-04/src/main.js
+++ modulo-02-04/04-04/src/main.js
@@ -1,13 +1,75 @@
-const myPromisse = () =>
-  new Promise((resolved, reject) => {
-    setTimeout(() => {
-      resolved("OK");
-    }, 2000);
-  });
+import api from "./api";
 
-// myPromisse().then(response => ...)
+class App {
+  constructor() {
+    this.repositories = [];
 
-async function execPromisse() {
-  const response = await myPromisse();
-  console.log(response);
+    this.formEl = document.getElementById("repo-form");
+    this.inputEl = document.querySelector("input[name=repository]");
+    this.listEl = document.getElementById("repo-list");
+
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formEl.onsubmit = event => this.addRepository(event);
+  }
+
+  async addRepository(event) {
+    event.preventDefault();
+
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.length === 0) return;
+
+    const response = await api.get(`/repos/${repoInput}`);
+
+    const {
+      name,
+      description,
+      html_url,
+      owner: { avatar_url }
+    } = response.data;
+
+    this.repositories.push({
+      name,
+      description,
+      avatar_url,
+      html_url
+    });
+
+    this.inputEl.value = "";
+
+    this.render();
+  }
+
+  render() {
+    this.listEl.innerHTML = "";
+
+    // Só percorre o array não o modifica, o map pode modificar
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement("img");
+      imgEl.setAttribute("src", repo.avatar_url);
+
+      let titleEl = document.createElement("strong");
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement("p");
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement("a");
+      linkEl.setAttribute("target", "_blank");
+      linkEl.appendChild(document.createTextNode("Acessar"));
+
+      let listItemEl = document.createElement("li");
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
+  }
 }
+
+new App();
