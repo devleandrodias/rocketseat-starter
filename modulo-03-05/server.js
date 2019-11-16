8eaf7b1e036f23ce689231e5e8b5723160ecb153
Leandro Dias <leandro.dias_profissional@outlook.com.br>
2019-11-16 10:41:47 -0300

    starter

--- modulo-03-05/server.js
+++ modulo-03-05/server.js
@@ -1,9 +1,28 @@
-const express = require('express');
-
+import express from 'express';
+import mongoose from 'mongoose';
+import './src/models/Product';
+// Iniciando a aplicação
 const app = express();
 
-app.get('/', (req, res) => {
+// Conectando o banco
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Meus models
+const Product = mongoose.model('Product');
+
+// Rotas da aplicação
+app.get('/new', (req, res) => {
+  Product.create({
+    title: 'React Native',
+    description: 'Interface gráfica para mobile',
+    url: 'https://github.com/facebook/react-native',
+  });
+
   return res.json({ ok: true });
 });
 
+// Servidor rodando
 app.listen(3001);
