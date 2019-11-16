b96d84e859964f1ea102e0a8f3ea78a4b088601e
Leandro Dias <leandro.dias_profissional@outlook.com.br>
2019-11-16 11:44:00 -0300

    starter

--- modulo-03-05/server.js
+++ modulo-03-05/server.js
@@ -5,6 +5,7 @@ require('./src/models/Product');
 const Product = mongoose.model('Product');
 // Iniciando a aplicação
 const app = express();
+app.use(express.json());
 
 // Conectando o banco
 mongoose.connect('mongodb://localhost:27017/nodeapi', {
@@ -12,9 +13,31 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
   useUnifiedTopology: true,
 });
 
-app.get('/', async (req, res) => {
+app.get('/products', async (req, res) => {
   const products = await Product.find();
   return res.json(products);
+});
+
+app.post('/products', async (req, res) => {
+  const product = await Product.create(req.body);
+  return res.json(product);
+});
+
+app.get('/products/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  return res.json(product);
+});
+
+app.put('/products/:id', async (req, res) => {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true, // product vem retornado o valor novo
+  });
+  return res.json(product);
+});
+
+app.delete('/products/:id', async (req, res) => {
+  await Product.findByIdAndRemove(req.params.id);
+  return res.json({ ok: 'Registro Deletado' });
 });
 
 // Servidor rodando
