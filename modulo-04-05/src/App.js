1b56a79301cb0fe070f30822415938026fd67956
Leandro Dias <leandro.dias_profissional@outlook.com.br>
2019-11-16 12:53:48 -0300

    starter

--- modulo-04-05/src/App.js
+++ modulo-04-05/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
+import Header from './components/Header';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React!</h1>
-    </div>
-  );
+export default function App() {
+  return <Header />;
 }
-
-export default App;
---
+++ modulo-04-05/src/components/Header/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './styles.css';
+
+export default function Header() {
+  return <header id="main-header">HuntJS</header>;
+}
