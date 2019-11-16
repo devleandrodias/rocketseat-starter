7ef356b84a8f496b63b08420be679ca93f80181b
Leandro Dias <leandro.dias_profissional@outlook.com.br>
2019-11-16 13:12:18 -0300

    starter

--- modulo-04-05/src/pages/main/index.js
+++ modulo-04-05/src/pages/main/index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+  state = {
+    products: [],
+  };
+
   // Toda função do react é escrita assim
   componentDidMount() {
     this.loadProducts();
@@ -11,10 +15,16 @@ export default class Main extends Component {
   loadProducts = async () => {
     const response = await api.get('/products');
 
-    console.log(response.data.docs);
+    this.setState({ products: response.data.docs });
   };
 
   render() {
-    return <h1>Hello Rocketseat</h1>;
+    return (
+      <div className="product-list">
+        {this.state.products.map(product => (
+          <h1 key={product._id}>{product.title}</h1>
+        ))}
+      </div>
+    );
   }
 }
