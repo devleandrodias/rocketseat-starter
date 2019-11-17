21497def12bdcf6fe208b66a53c1edcff73beece
Leandro Dias <leandro.dias_profissional@outlook.com.br>
2019-11-17 16:12:36 -0300

    starter

--- modulo-05-05/hunt/src/pages/main.js
+++ modulo-05-05/hunt/src/pages/main.js
@@ -1,15 +1,36 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import api from '../service/api';
 
 export default class Main extends Component {
   static navigationOptions = {
     title: 'JS Hunt',
   };
 
+  state = {
+    docs: [],
+  };
+
+  UNSAFE_componentWillMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    const response = await fetch(
+      'https://rocketseat-node.herokuapp.com/api/products',
+    );
+
+    const {docs} = response.data;
+    this.setState(docs);
+  };
+
   render() {
     return (
       <View>
-        <Text>Página Principal App!</Text>
+        <Text>Página Inicial</Text>
+        {this.state.docs.map(product => (
+          <Text key={product._id}>{product.title}</Text>
+        ))}
       </View>
     );
   }
---
+++ modulo-05-05/hunt/src/service/api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const api = axios({
+  baseURL: '',
+});
+
+export default api;
